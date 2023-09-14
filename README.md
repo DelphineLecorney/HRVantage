4dd83a41ff357fc4eb28c49f4b0ecf08eb7af335
DelphineLecorney <lecorney.delphine@gmail.com>
2023-09-14 14:49:54 +0200

    Add table training

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+Work in progress
+
 GRH_IA_DB :  Base de données de gestion des ressources humaines basée sur l'IA.
--- database/migrations/2023_09_14_124617_create_training_table.php
+++ database/migrations/2023_09_14_124617_create_training_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('vacations', function (Blueprint $table) {
+        Schema::create('training', function (Blueprint $table) {
             $table->id();
             $table->foreignId('employee_id')->constrained();
             $table->string('name', 100);
@@ -18,6 +19,9 @@ return new class() extends Migration {
             $table->date('start_date');
             $table->date('end_date');
             $table->string('document', 100);
+            $table->string('status', 100);
+            $table->string('type', 100);
+            $table->string('description', 100);
             $table->timestamps();
         });
     }
@@ -27,6 +31,6 @@ return new class() extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('vacations');
+        Schema::dropIfExists('training');
     }
 };
