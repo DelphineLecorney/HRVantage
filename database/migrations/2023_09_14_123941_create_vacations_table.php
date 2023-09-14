8949eda1cc9a0bd710337a39aee550db4ea9a4de
DelphineLecorney <lecorney.delphine@gmail.com>
2023-09-14 14:44:03 +0200

    Add table vacations

---
+++ database/migrations/2023_09_14_123941_create_vacations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vacations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status', 100);
+            $table->string('type', 100);
+            $table->string('document', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vacations');
+    }
+};
