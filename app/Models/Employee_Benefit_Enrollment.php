81d1415754ab014693e9c019d7383da8991558a3
DelphineLecorney <lecorney.delphine@gmail.com>
2023-09-14 16:51:32 +0200

    Modify model Employee_Benefit_Enrollment

--- app/Models/Employee_Benefit_Enrollment.php
+++ app/Models/Employee_Benefit_Enrollment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Employee_Benefit_Enrollment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'employee_id',
+        'benefit_id',
+        'enrollment_date',
+        'is_enrolled',
+    ];
 }
