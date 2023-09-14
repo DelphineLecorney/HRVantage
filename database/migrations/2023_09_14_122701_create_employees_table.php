cb36c527952dc5e5ab432ac6343458d63b29ab45
DelphineLecorney <lecorney.delphine@gmail.com>
2023-09-14 14:33:29 +0200

    Add table employees

---
+++ database/migrations/2023_09_14_122701_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('surname', 100);
+            $table->string('email', 100)->unique();
+            $table->string('phone', 100);
+            $table->string('address', 100);
+            $table->string('city', 100);
+            $table->string('zip_code', 100);
+            $table->string('country', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
