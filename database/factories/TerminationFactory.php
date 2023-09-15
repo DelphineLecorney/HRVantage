7068a2ac8399502d869632154bdb516ae32c48e9
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-15 20:24:06 +0200

    Create db with seeders

--- database/factories/TerminationFactory.php
+++ database/factories/TerminationFactory.php
@@ -17,8 +17,8 @@ class TerminationFactory extends Factory
     public function definition(): array
     {
         return [
-            'employee_id' => $this->faker->randomNumber(),
-            'position_id' => $this->faker->randomNumber(),
+            'employee_id' => $this->faker->randomNumber(1, 50),
+            'position_id' => $this->faker->randomNumber(1, 10),
             'date' => $this->faker->date(),
             'comment' => $this->faker->text(100),
         ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,11 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Employee;
-use App\Models\Employee_Comments;
-use App\Models\Employee_Document;
 use App\Models\Position;
 use App\Models\Recruitment;
-use App\Models\Salary_history;
 use App\Models\SocialBenefit;
 use App\Models\Termination;
 use App\Models\Training;
@@ -29,9 +26,6 @@ class DatabaseSeeder extends Seeder
         Employee::factory()->count(50)->create();
         Vacation::factory()->count(10)->create();
         Training::factory()->count(50)->create();
-        Salary_history::factory()->count(10)->create();
-        Employee_Comments::factory()->count(10)->create();
-        Employee_Document::factory()->count(10)->create();
         Termination::factory()->count(10)->create();
     }
 }
