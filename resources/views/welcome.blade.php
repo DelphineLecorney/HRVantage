632f78454c78fdfd575ec80543599fa0c957f8a5
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-16 10:45:36 +0200

    Translation of the page into English

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion Admin</title>
+    <title>Admin Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
@@ -11,19 +11,19 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Connexion Admin') }}</div>
+                    <div class="card-header">{{ __('Admin Login') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('welcome') }}">
                             @csrf
 
                             <div class="form-group">
-                                <label for="email">{{ __('Adresse E-mail') }}</label>
+                                <label for="email">{{ __('E-mail address') }}</label>
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                             </div>
 
                             <div class="form-group">
-                                <label for="password">{{ __('Mot de passe') }}</label>
+                                <label for="password">{{ __('Password') }}</label>
                                 <input id="password" type="password" class="form-control" name="password" required>
                             </div>
 
@@ -31,14 +31,14 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Se souvenir de moi') }}
+                                        {{ __('Remember me') }}
                                     </label>
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Connexion') }}
+                                    {{ __('Connection') }}
                                 </button>
                             </div>
                         </form>
