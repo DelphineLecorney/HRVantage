7410dad15579639667aab73a0d9f8ffbde953da9
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-16 10:42:35 +0200

    Add some routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/', function () {
-    return view('welcome');
 })->name('welcome');
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+});
