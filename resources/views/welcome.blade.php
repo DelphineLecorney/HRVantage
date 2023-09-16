fdf63d5fdc3e66c26193e667cb196212a52b1427
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-16 10:59:17 +0200

    Center the container on welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
+    <br><br><br><br><br><br><br><br><br><br><br><br>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
