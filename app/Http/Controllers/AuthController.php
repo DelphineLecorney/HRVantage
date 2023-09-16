8d7694e10bbec1aaf3ec894babeab22e163cfc8d
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-16 10:36:35 +0200

    Add comments

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,16 +6,25 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    // 'showRegistrationForm' method: Displays the registration form.
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
 
-    public function register()
+    // 'register' method: Validates the user's input and creates a new user.
+    public function register(Request $request)
     {
-        return view('auth.register');
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'firstname' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'phone' => 'required|numeric',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
     }
 
+    // 'showLoginForm' method: Displays the login form.
     public function login(Request $request)
     {
         $request->validate([
