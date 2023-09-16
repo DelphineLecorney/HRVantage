58045e23f3f9892cce3e76a8840ac796c151d736
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-16 10:25:49 +0200

    Add AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+    }
+}
