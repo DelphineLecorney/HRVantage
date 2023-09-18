e063de6fbf88487da613c4f2b737ca6de8af54c6
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-18 22:27:49 +0200

    Modify welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,33 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HRVantage - Système de Gestion des Ressources Humaines</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <header>
-        <h1>Bienvenue dans HRVantage</h1>
-        <p>Votre Solution de Gestion des Ressources Humaines</p>
-    </header>
+<header class="jumbotron text-center">
+    <h1 class="display-4">Bienvenue dans HRVantage</h1>
+    <p class="lead">Votre Solution de Gestion des Ressources Humaines</p>
+</header>
 
-    <main>
-        <section id="description">
-            <h2>Description</h2>
-            <p>HRVantage est un système complet de gestion des ressources humaines conçu pour simplifier les processus RH, gérer les informations sur les employés et faciliter les opérations RH.</p>
-        </section>
+<main class="container">
+    <section id="description">
+        <h2 class="mt-5">Description</h2>
+        <p>HRVantage est un système complet de gestion des ressources humaines conçu pour simplifier les processus RH, gérer les informations sur les employés et faciliter les opérations RH.</p>
+    </section>
 
-        <section id="features">
-            <h2>Fonctionnalités</h2>
-            <ul>
-                <li>Gestion des employés</li>
-                <li>Gestion des postes</li>
-                <li>Gestion des avantages sociaux</li>
-            </ul>
-        </section>
+    <section id="features">
+        <h2 class="mt-5">Fonctionnalités</h2>
+        <ul class="list-group">
+            <li class="list-group-item">Gestion des employés</li>
+            <li class="list-group-item">Gestion des postes</li>
+            <li class="list-group-item">Gestion des avantages sociaux</li>
+        </ul>
+    </section>
+</main>
 
-    </main>
+<footer class="bg-dark text-light text-center py-3">
+    <p>&copy; 2023 HRVantage. Tous droits réservés.</p>
+</footer>
 
-    <footer>
-        <p>&copy; 2023 HRVantage. Tous droits réservés.</p>
-    </footer>
 </body>
 </html>
