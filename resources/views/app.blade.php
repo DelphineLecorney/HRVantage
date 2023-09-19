214b34e486738b1278ec430a4c6a3fa12e823ca5
DelphineLecorney <lecorney.delphine@gmail.com>
2023-09-19 09:25:07 +0200

    Add file app.blade

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HRVantage - Système de Gestion des Ressources Humaines</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/about">À propos</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <p>&copy; 2023 HRVantage. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
