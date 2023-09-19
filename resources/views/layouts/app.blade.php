8c14e30f6023cb1910e30e7ee8a75a4e2c67653b
DelphineLecorney <lecorney.delphine@gmail.com>
2023-09-19 14:20:33 +0200

    Modify login and welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <title>HRVantage - Système de Gestion des Ressources Humaines</title>
 </head>
 <body>
@@ -10,7 +11,7 @@
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="http://127.0.0.1:8000">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -51,7 +52,7 @@
     <main>
         @yield('content')
     </main>
-    <footer>
+    <footer class="mt-auto py-3 bg-light">
         <p>&copy; 2023 HRVantage. Tous droits réservés.</p>
     </footer>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HRVantage - Système de Gestion des Ressources Humaines</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
+    
 <body>
     @section('content')
 <header class="jumbotron text-center">
