ce74252edd4db6f58ee7b019358e79e66c648d24
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-22 10:26:35 +0200

    Add PositionController and position.blade.php

---
+++ app/Http/Controllers/PositionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Position;
+
+class PositionController extends Controller
+{
+    // 'index' method: Displays the positions.
+    public function index()
+    {
+        $positions = Position::all();
+        return view('positions.index', compact('positions'));
+    }
+}
