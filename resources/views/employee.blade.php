871f5ccae054112e1dd3a59e1597539537d16421
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-23 18:01:28 +0200

    Modify employee.blade

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-14">
             <div class="card">
                 <div class="card-header">
                     <h1 class="card-title">Employee List</h1>
@@ -14,6 +14,11 @@
                             <tr>
                                 <th scope="col">Name</th>
                                 <th scope="col">Email</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Adress</th>
+                                <th scope="col">City</th>
+                                <th scope="col">Zip Code</th>
+                                <th scope="col">Country</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -21,6 +26,11 @@
                             <tr>
                                 <td>{{ $employee->name }}</td>
                                 <td>{{ $employee->email }}</td>
+                                <td>{{ $employee->phone }}</td>
+                                <td>{{ $employee->address }}</td>
+                                <td>{{ $employee->city }}</td>
+                                <td>{{ $employee->zip_code }}</td>
+                                <td>{{ $employee->country }}</td>
                             </tr>
                             @endforeach
                         </tbody>
