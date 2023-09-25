6c947cc3282622d7158e34ed60662834f5c63a9b
DelphineLecorney <lecorney.delphine@gmail.com>
2023-09-25 11:34:42 +0200

    Modify position.blade

--- resources/views/position.blade.php
+++ resources/views/position.blade.php
@@ -3,12 +3,41 @@
 @yield('content')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-            <h1>Position</h1>
-            <p>Position</p>
-            </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <br>
+            <h1>Postes existants dans notre entreprise</h1>
+            <br>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <h2>Titre du poste 1</h2>
+                    <p>Description du poste 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </li>
+                <li class="list-group-item">
+                    <h2>Titre du poste 2</h2>
+                    <p>Description du poste 2 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </li>
+                <li class="list-group-item">
+                    <h2>Titre du poste 3</h2>
+                    <p>Description du poste 3 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </li>
+                <li class="list-group-item">
+                    <h2>Titre du poste 4</h2>
+                    <p>Description du poste 4 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </li>
+                <li class="list-group-item">
+                    <h2>Titre du poste 5</h2>
+                    <p>Description du poste 5 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </li>
+                <li class="list-group-item">
+                    <h2>Titre du poste 6</h2>
+                    <p>Description du poste 6 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </li>
+
+            </ul>
+
         </div>
     </div>
+</div>
 @endsection
