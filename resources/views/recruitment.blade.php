81cc87b04f97345a277fbf2609a31f7dbb21f7b6
DelphineLecorney <lecorney.delphine@gmail.com>
2023-09-25 11:42:36 +0200

    Modify recruitment.blade

--- resources/views/recruitment.blade.php
+++ resources/views/recruitment.blade.php
@@ -3,12 +3,59 @@
 @yield('content')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-            <h1>Recruitment</h1>
-            <p>Recruitment</p>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>Suivi du Recrutement</h1>
+
+            <div class="card">
+                <div class="card-header">
+                    <h2>Poste 1 - Candidat 1</h2>
+                </div>
+                <div class="card-body">
+                    <h5>Étape actuelle: CV reçu</h5>
+                    <p>Date de soumission du CV: 10/01/2023</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <h2>Poste 1 - Candidat 2</h2>
+                </div>
+                <div class="card-body">
+                    <h5>Étape actuelle: CV reçu</h5>
+                    <p>Date de soumission du CV: 11/01/2023</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <h2>Poste 1 - Candidat 3</h2>
+                </div>
+                <div class="card-body">
+                    <h5>Étape actuelle: CV reçu</h5>
+                    <p>Date de soumission du CV: 12/01/2023</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <h2>Poste 2 - Candidat 2</h2>
+                </div>
+                <div class="card-body">
+                    <h5>Étape actuelle: CV reçu</h5>
+                    <p>Date de soumission du CV: 10/01/2023</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <h2>Poste 3 - Candidat 3</h2>
+                </div>
+                <div class="card-body">
+                    <h5>Étape actuelle: Entretien téléphonique</h5>
+                    <p>Date de l'entretien: 15/01/2023</p>
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
