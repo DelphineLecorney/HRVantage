f0ea36044a7b99e10c0a07a288d92dff3bc418b4
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-27 17:49:23 +0200

    Add 'tm' on footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,8 @@
   </main>
   <footer class="footer mt-auto py-3 bg-light">
     <div class="justify-content-end text-center">
-      <p>&copy; 2023 HRVantage. All rights reserved.</p>
+      <p>&copy; <?php echo date("Y"); ?> 2023 HRVantage &trade; &nbsp; All rights reserved. &nbsp; &nbsp; Want to join us &nbsp; &nbsp; <a class="btn btn-outline-secondary" href="{{ route('recruitment.create') }}">Jobs</a></p>
+
     </div>
   </footer>
 </body>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="footer mt-auto py-3 bg-light">
     <div class="justify-content-end text-center">
-        <p>&copy; 2023 HRVantage. &nbsp; All rights reserved. &nbsp; Want to join us &nbsp; <a class="btn btn-outline-secondary" href="{{ route('recruitment.create') }}">Jobs</a></p>
+        <p>&copy; <?php echo date("Y"); ?> 2023 HRVantage &trade; &nbsp; All rights reserved. &nbsp; &nbsp; Want to join us &nbsp; &nbsp; <a class="btn btn-outline-secondary" href="{{ route('recruitment.create') }}">Jobs</a></p>
+
     </div>
 </footer>
