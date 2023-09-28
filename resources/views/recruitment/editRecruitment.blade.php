59c1d1fb1b8c9c802ebd3d657bde27a9fae4992d
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-09-28 10:29:28 +0200

    Modify button and add br for spaces div

--- resources/views/recruitment/editRecruitment.blade.php
+++ resources/views/recruitment/editRecruitment.blade.php
@@ -14,38 +14,47 @@
                 @endforeach
             </select>
         </div>
+        <br>
         <div class="form-group">
             <label for="name">Name</label>
             <input type="text" name="name" id="name" value="{{ $recruitment->name }}" autocomplete="name" class="form-control" required>
         </div>
+        <br>
         <div class="form-group">
             <label for="firstname">Firstname</label>
             <input type="text" name="firstname" id="firstname" value="{{ $recruitment->firstname }}" class="form-control" required>
         </div>
+        <br>
         <div class="form-group">
             <label for="email">Email</label>
             <input type="text" name="email" id="email" autocomplete="name" value="{{ $recruitment->email }}" class="form-control" required>
         </div>
+        <br>
         <div class="form-group">
             <label for="phone">Phone</label>
             <input type="text" name="phone" id="phone" autocomplete="name" value="{{ $recruitment->phone }}" class="form-control" required>
         </div>
+        <br>
         <div class="form-group">
             <label for="address">Address</label>
             <input type="text" name="address" id="address" autocomplete="name" value="{{ $recruitment->address }}" class="form-control" required>
         </div>
+        <br>
         <div class="form-group">
             <label for="city">City</label>
             <input type="text" name="city" id="city" autocomplete="name" value="{{ $recruitment->city }}" class="form-control" required>
         </div>
+        <br>
         <div class="form-group">
             <label for="zip_code">Zip Code</label>
             <input type="text" name="zip_code" id="zip_code" value="{{ $recruitment->zip_code }}" autocomplete="name" class="form-control" required>
         </div>
+        <br>
         <div class="form-group">
             <label for="country">Country</label>
             <input type="text" name="country" id="country" value="{{ $recruitment->country }}" autocomplete="name" class="form-control" required>
         </div>
+        <br>
         <div class="form-group">
             <label for="status">Status</label>
             <select name="status" id="status" value="{{ $recruitment->status }}" class="form-control">
@@ -54,16 +63,19 @@
                 <option value="Rejected">Rejected</option>
             </select>
         </div>
+        <br>
         <div class="form-group">
-            <button type="submit" id="submit" class="btn btn-primary">Request for edit Recruitment</button>
+            <button type="submit" id="submit" class="btn btn-primary">Request for edit Recruitment</button> &nbsp; &nbsp; &nbsp; &nbsp;
+
+            <form action="{{ route('recruitment.destroy', ['recruitment' => $recruitment]) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
         </div>
     </form>
 
-    <form action="{{ route('recruitment.destroy', ['recruitment' => $recruitment]) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">Delete</button>
-    </form>
+
 
 </div>
 @endsection
