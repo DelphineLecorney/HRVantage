ab0466576f204255291e9370238abf741405ea05
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-10-01 18:22:08 +0200

    Add functions create employeeController

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
@@ -12,5 +13,19 @@ class EmployeeController extends Controller
         $employees = Employee::all();
 
         return view('employee', compact('employees'));
+    }
+
+    // 'create' method: Displays the create form.
+    public function create()
+    {
+        return view('createEmployee');
+    }
+
+    // 'store' method: Creates a new employee.
+    public function store(Request $request)
+    {
+        Employee::create($request->all());
+
+        return redirect()->route('employee')->with('success', 'The employee has been registered successfully.');
     }
 }
