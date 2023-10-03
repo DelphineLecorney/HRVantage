df82cb0517ed56f18a94e98e03555021cc7b177e
DelphineLecorney <lecorney.delphine@gmail.com>
2023-10-03 16:55:06 +0200

    Test in production

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>HRVantage - Human Resources Management System</title>
+
 <body>
-<main class="min-vh-100 d-flex justify-content-center align-items-center">
+    <main class="min-vh-100 d-flex justify-content-center align-items-center">
         <div class="card">
             <div class="card-header">{{ __('Admin Login') }}</div>
             <div class="card-body">
@@ -37,11 +38,11 @@
         </div>
     </main>
     <footer class="footer mt-auto py-3 bg-light">
-    <div class="justify-content-end text-center">
-        <p>&copy; 2023 HRVantage. All rights reserved.</p>
-    </div>
-</footer>
-</footer>
+        <div class="justify-content-end text-center">
+            <p>&copy; 2023 HRVantage. All rights reserved.</p>
+        </div>
+    </footer>
+    </footer>
 </body>
-</html>
 
+</html>
